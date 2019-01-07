e44b4747ecbf06ba1ac93cb635f378d5319650de
yanghai <yanghai@ablecloud.cn>
2019-01-08 00:55:02 +0800

    初始化工程，调整Cmake.list文件的BUG

---
+++ binTree/init.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 杨海 on 2019-01-08.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int init() {
+   cout<<"init binTree"<<endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+extern int init();
+int main() {
+    init();
+    cout << "Hello, World!" << endl;
+    return 0;
+}
